6ff41b6787636050b8954ec167c09252f93ac1a4
Jake Soenneker <jake@soenneker.com>
2025-03-10 08:42:40 -0500

    Initial commit

---
+++ src/Registrars/DNSimpleSuiteRegistrar.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Soenneker.DNSimple.Delegation.Registrars;
+using Soenneker.DNSimple.Domains.Registrars;
+using Soenneker.DNSimple.Identity.Registrars;
+using Soenneker.DNSimple.Registrar.Registrars;
+
+namespace Soenneker.DNSimple.Suite.Registrars;
+
+/// <summary>
+/// A comprehensive suite of utility libraries for DNSimple's API
+/// </summary>
+public static class DNSimpleSuiteRegistrar
+{
+    /// <summary>
+    /// Adds the DNSimple suite as a singleton service. <para/>
+    /// </summary>
+    public static IServiceCollection AddDNSimpleSuiteAsSingleton(this IServiceCollection services)
+    {
+        services.AddDNSimpleRegistrarUtilAsSingleton()
+                .AddDNSimpleDelegationUtilAsSingleton()
+                .AddDNSimpleDomainsUtilAsSingleton()
+                .AddDNSimpleIdentityUtilAsSingleton();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds  the DNSimple suite as a scoped service. <para/>
+    /// </summary>
+    public static IServiceCollection AddDNSimpleSuiteAsScoped(this IServiceCollection services)
+    {
+        services.AddDNSimpleRegistrarUtilAsScoped()
+                .AddDNSimpleDelegationUtilAsScoped()
+                .AddDNSimpleDomainsUtilAsScoped()
+                .AddDNSimpleIdentityUtilAsScoped();
+
+        return services;
+    }
+}
---
+++ test/Soenneker.DNSimple.Suite.Tests/DNSimpleSuiteTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.DNSimple.Suite.Tests;
+
+[Collection("Collection")]
+public class DNSimpleSuiteTests : FixturedUnitTest
+{
+    public DNSimpleSuiteTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
